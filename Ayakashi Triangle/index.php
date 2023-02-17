8d7204b10c643f03ebf883fa2016316ac8d510e9
akumaid <125697847+akumaid@users.noreply.github.com>
2023-02-18 04:28:34 +0700

    Create index.php

---
+++ Ayakashi Triangle/index.php
@@ -0,0 +1,13 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '<link rel="stylesheet" href="/src/css/sty.css" />';
+echo '<title>Akuma</title>';
+echo '<meta http-equiv="refresh" content="2; url = https://www.akuma.my.id/2023/01/ayakashi-triangle.html" />';
+echo '</head>';
+echo '<body>';
+echo '';
+echo '</body>';
+echo '</html>';
+?>
